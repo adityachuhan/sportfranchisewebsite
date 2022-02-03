fbb59a1b19a797c1df41897900124bc34b1ed3fe
Aditya <adtyachauhan7@gmail.com>
2022-02-03 19:25:11 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,8 @@
+function hm() {
+    var button = document.getElementById('search')
+    if (button.style.display != 'none') {
+        button.style.display = 'none';
+    } else {
+        button.style.display = 'block';
+    }
+}
